9c72684de6fa3d3a4c2966735fb5c42276ba66c8
sammck26 <113983793+sammck26@users.noreply.github.com>
2024-09-29 14:27:35 +0100

    Attemting to get first webpage running

--- src/server.js
+++ src/server.js
@@ -1,14 +1,7 @@
-const express = require('express');
-
-const app = express();
+const app = require('./app.js');  // Import the app setup from app.js
 const port = 3000;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
