8cc4579bc9d7874f997d341a9207cda7d21ca166
Fin Bates <c2086284@hallam.shu.ac.uk>
2024-09-30 14:04:26 +0100

    updated controller
    ..

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,4 @@
 exports.getDashboard = (req, res) => { 
     const userData = {};  // Logic to fetch user dashboard data
-    res.render('/user/user_dashboard', { user: userData }); 
+    res.render('user_dashboard', { user: userData }); 
   };
