2794ca83bf3551fdb791b8b69b7da1d1806e19f9
sammck26 <113983793+sammck26@users.noreply.github.com>
2024-10-03 18:52:16 +0100

    Working landing page

--- src/app.js
+++ src/app.js
@@ -7,13 +7,17 @@ app.set('view engine', 'ejs');
 
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
-console.log('Views directory set to:', app.get('views'));  // Debugging statement
+//console.log('Views directory set to:', app.get('views'));  // Debugging statement
 
 // Middleware for json
 app.use(express.json());
 
+
+
 // Use routes
+const landingRoutes = require('./routes/landingpage.js'); // Import landing page routes
 const userRoutes = require('./routes/user.js');
+app.use('/', landingRoutes);
 app.use('/user', userRoutes);  // puts user-related routes under /user
 
 module.exports = app;  // Export the app setup
