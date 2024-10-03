018cbf6203c7c3ac856312d89b4d5d8091f662d7
sammck26 <113983793+sammck26@users.noreply.github.com>
2024-10-03 14:10:06 +0100

    Fixed can't find view directory error

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-
+// Set the view engine
 app.set('view engine', 'ejs');
 
 // Set the views directory
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
+console.log('Views directory set to:', app.get('views'));  // Debugging statement
 
 // Middleware for json
 app.use(express.json());
 
 // Use routes
 const userRoutes = require('./routes/user.js');
-app.use('/user', userRoutes);  // puts user related routes under /user
-
-
+app.use('/user', userRoutes);  // puts user-related routes under /user
 
 module.exports = app;  // Export the app setup
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,5 @@
 exports.getDashboard = (req, res) => { 
     const userData = {};  // will fetch user dashboard data from database
     res.render('user/user_dashboard', { user: userData }); 
-    //console.log('User dashboard data sent');
+    console.log('User dashboard data sent');
   };
