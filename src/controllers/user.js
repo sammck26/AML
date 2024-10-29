30f558994349ef4e63198b51c3d415426b68e647
Fin Bates <c2086284@hallam.shu.ac.uk>
2024-10-29 14:03:04 +0000

    profile controlled
    profile navgation

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,4 +6,13 @@ exports.getDashboard = (req, res) => {
     res.render('user/user_dashboard', { user: userData }); 
     //console.log('User dashboard data sent');
 
+};
+
+exports.getProfile = (req, res) => {
+  //const userData = {};  // will fetch user dashboard data from database
+  const userData = {
+    role: "customer", // Hardcoded for now, but in a real app, you would get this from the logged-in user session or database
   };
+  res.render("user/view_profile", { user: userData });
+  //console.log('User dashboard data sent');
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,7 @@ const viewInventoryController = require('../controllers/view_inventory.js');
 
 // User-related routes
 router.get('/dashboard', userController.getDashboard);  // URL: /user/dashboard
+router.get("/profile", userController.getProfile);
 
 router.get('/inventory', viewInventoryController.viewInventory);
 module.exports = router;
