7b9c8c7688af5904262190675c51a4fc3da65a93
Fin Bates <c2086284@hallam.shu.ac.uk>
2024-11-05 14:51:03 +0000

    register and login

--- src/controllers/landingpage.js
+++ src/controllers/landingpage.js
@@ -9,3 +9,18 @@ exports.getLandingPage = (req, res) => {
   res.render("landingpage/landing", { user: userData, activePage: "landing" }); 
     //console.log('Landing page rendered');
 };
+
+exports.getLoginPage = (req, res) => { // Sample data
+  const userData = {
+    role: "guest", // Hardcoded for now, but in a real app, you would get this from the logged-in user session or database
+  };
+  res.render("landingpage/login", { user: userData, activePage: "login" });
+};
+
+exports.getRegisterPage = (req, res) => {
+  // Sample data
+  const userData = {
+    role: "guest", // Hardcoded for now, but in a real app, you would get this from the logged-in user session or database
+  };
+  res.render("landingpage/register", { user: userData, activePage: "register" });
+};
--- src/routes/landingpage.js
+++ src/routes/landingpage.js
@@ -5,6 +5,8 @@ const landingController = require('../controllers/landingpage.js');
 
 // Route to render the landing page
 router.get('/', landingController.getLandingPage);
+router.get('/landingpage/login', landingController.getLoginPage);
+router.get('/landingpage/register', landingController.getRegisterPage);
 
 
 module.exports = router;
