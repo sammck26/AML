206886f5ab06001afcacddfc7896356319af53a2
sammck26 <113983793+sammck26@users.noreply.github.com>
2024-11-05 14:14:00 +0000

    working on none-embedded database

--- db/models/db.js
+++ db/models/db.js
@@ -1,3 +1,27 @@
-// src/models/db.js
-const sqlite3 = require('sqlite3').verbose();
-const path = require('C:\Users\mckib\OneDrive\Documents\GitHub\AML\db\Database.db');
+// db.js
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config(); 
+
+// creating a connection to the database
+const pool = mysql.createPool({
+  host: process.env.host, 
+  user: process.env.user, 
+  password: process.env.password, 
+  database: process.env.database, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+pool.query("SELECT * FROM customer"), (err, results, fields) => {
+  if(err) {
+    console.log("dsi bicth ain wokrin",err); 
+  }
+  else {
+    console.log(results); 
+  }
+
+}
+// Export the pool for use in other files
+module.exports = pool.promise();
