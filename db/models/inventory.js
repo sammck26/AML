a4a523b1f66b39b1bbb9959ca978b142a7d2c52e
sammck26 <113983793+sammck26@users.noreply.github.com>
2024-11-12 14:56:03 +0000

    tyring to add crud

--- db/models/inventory.js
+++ db/models/inventory.js
@@ -7,11 +7,21 @@ const dotenv = require('dotenv').config({
 mongoose.connect(process.env.MONGO_URI, { //uses mongoose to connect 
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }).then(() => {
+  }).then(async() => {
     console.log("Connected to MongoDB Atlas with Mongoose");
-  }).catch((err) => {
-    console.error("Failed to connect to MongoDB Atlas:", err);
-  });
+    const collections = await mongoose.connection.db.listCollections().toArray();
+    const mediaCollection = collections.find((c) => c.name === "media");
+
+    if (mediaCollection) {
+      console.log("Media collection found:", mediaCollection.name);
+    } else {
+      console.log("Media collection not found.");
+    }
+  })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB Atlas:", err);
+    });
+
 
 const MediaSchema = new mongoose.Schema({
   media_title: { type: String, required: true },
@@ -22,7 +32,7 @@ const MediaSchema = new mongoose.Schema({
 
 const GenreSchema = new mongoose.Schema({
   genre_description: { type: String, required: true }, 
-  genre_id: { type: Number, required: true, unique: true }, 
+  //id: { type: mongoose.Schema.Types.ObjectId, ref: "Genre" },
 });
 
 const Media = mongoose.model("Media", MediaSchema);
@@ -34,8 +44,8 @@ MediaSchema.methods.isAvailable = function () { // if the amount is more than ze
 
 
 Media.find()
-  .populate({
-    path: "genre_id",
+  .populate({ 
+    path: "genre_id", 
     select: "genre_description", // populate genre_description from Genre
   })
   .then((mediaItems) => {
@@ -45,7 +55,8 @@ Media.find()
         availability: item.isAvailable(), // include isAvailable status for each media item
       });
     });
-    console.log(mediaItems);
+    console.log("Media items", mediaItems);
+    console.log("genre_id", GenreSchema);
   })
   .catch((error) => {
     console.error(error);
