32e0cf7ff44bb200b34e193a6b2cf0097f199686
Fin Bates <c2086284@hallam.shu.ac.uk>
2024-12-09 16:14:15 +0000

    testiing

--- test/media.test.js
+++ test/media.test.js
@@ -1,86 +1,21 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../app"); // Path to your Express app
-const { Media } = require("../db/models/inventory");
-const { expect } = chai;
+const request = require("supertest");
+const app = require("../src/app");
+const axios = require("axios");
 
-chai.use(chaiHttp);
+jest.mock("axios"); // Mock axios for the image search
 
-describe("Manage Media - Librarian Features", () => {
-  let testMediaId;
-
-  // Test for adding a book
-  describe("POST /branch_librarian/addBook", () => {
-    it("should add a new book", (done) => {
-      const newBook = {
-        media_title: "Test Book",
-        author: "John Doe",
-        genre_id: "5f8d0d55b54764421b7156bc", // Replace with a valid genre ID from your database
-        quant: 10,
-      };
-
-      chai
-        .request(app)
-        .post("/branch_librarian/addBook")
-        .send(newBook)
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.text).to.include("redirect"); // Check for redirection
-          Media.findOne({ media_title: newBook.media_title }).then((media) => {
-            expect(media).to.exist;
-            testMediaId = media._id; // Save ID for other tests
-            done();
-          });
-        });
+it("should add a new book", async () => {
+  const response = await request(app)
+    .post("/branch_librarian/inventory/new?_id=6751ad6f03c78612cda4600e")
+    .send({
+      media_title: "Test Book",
+      author: "Test Author",
+      genre_id: "673358a98c529a6a6ec66a65",
+      quant: 10,
     });
-  });
 
-  // Test for fetching media details for update
-  describe("GET /branch_librarian/updateBook/:id", () => {
-    it("should fetch the media item to update", (done) => {
-      chai
-        .request(app)
-        .get(`/branch_librarian/updateBook/${testMediaId}`)
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.text).to.include("Test Book"); // Check if the book title appears in the response
-          done();
-        });
-    });
-  });
-
-  // Test for updating media quantity
-  describe("PUT /branch_librarian/updateMediaQuantity/:id", () => {
-    it("should update the media quantity", (done) => {
-      const updatedData = { newQuantity: 20 };
-
-      chai
-        .request(app)
-        .put(`/branch_librarian/updateMediaQuantity/${testMediaId}`)
-        .send(updatedData)
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          Media.findById(testMediaId).then((media) => {
-            expect(media.quant).to.equal(20);
-            done();
-          });
-        });
-    });
-  });
-
-  // Test for deleting media
-  describe("DELETE /branch_librarian/deleteMedia/:id", () => {
-    it("should delete the media item", (done) => {
-      chai
-        .request(app)
-        .delete(`/branch_librarian/deleteMedia/${testMediaId}`)
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          Media.findById(testMediaId).then((media) => {
-            expect(media).to.be.null;
-            done();
-          });
-        });
-    });
-  });
+  expect(response.status).toBe(302); // Expect the redirect status
+  expect(response.headers.location).toBe(
+    "/branch_librarian/librarianInventory?_id=6751ad6f03c78612cda4600e"
+  ); // Verify the redirect URL
 });
